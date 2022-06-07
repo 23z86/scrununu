1b72c53d5fde40b737fc5c8d03ab1b4a85f244c3
23z86 <71661529+23z86@users.noreply.github.com>
2022-06-07 20:40:28 +0200

    Add files via upload

--- functions.py
+++ functions.py
@@ -5,9 +5,9 @@
       |  |      |   ' |   | |    | |   | |    | |   |
  \___.'   `._.' /     `._/| /    | `._/| /    | `._/|
 
-build.........2
+build.........3
 status........alpha
-date..........20220605
+date..........20220607
 author........23z86
 language......DE/Python3
 gitpage.......https://github.com/23z86/scrununu
@@ -16,67 +16,93 @@ description...Simple web scraper for kununu.com
 ..............where employees can rate their employer anonymously
                                                                                 
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-
+import sys
 import requests
 from lxml import html
 import os.path
 import globals
 
-def output_file(i_file, i_url, i_xpath, i_first_page, i_last_page, i_sort):
+print(f'{globals.count_reviews} entries expected...')
+
+
+def output_file(i_file, i_xpath, i_option, i_url=globals.url, i_first_page=globals.first_page, i_last_page=globals.last_page, i_sort=globals.sort):
+
+    if i_url[-1:] != "/":
+        i_url = i_url + "/"
+
     file = open(i_file, "w")
-    
-    print(f'Writing {i_file}, please wait...')
-    
+
+    if i_option == 'file':
+        print(f'Writing {i_file}, please wait...')
+
     for page in range(i_first_page, i_last_page):
         temp_url = i_url + str(page) + i_sort
         request = requests.get(temp_url)
-        tree = html.fromstring(request.content)        
-    
-        section = tree.xpath(i_xpath)
-        
-        for sections in section:
-            file.write(sections+"\n")  
+        tree = html.fromstring(request.content)
 
-    file.close()    
+        section = tree.xpath(i_xpath)
+
+        for sections in section:
+            if i_file == globals.file_date:
+                sections = sections[:10]
+
+            if i_option == 'cli':
+                print(sections)
+
+            file.write(sections + "\n")
+
+    file.close()
+
+    if os.stat(i_file).st_size == 0:
+        os.remove(i_file)
+
 
 def get_company(i_url, i_xpath):
     request = requests.get(i_url)
-    tree = html.fromstring(request.content)  
-    
-    print(tree.xpath(i_xpath))
-    
-def write_data(i_file, i_url, i_xpath, i_first_page, i_last_page, i_sort):
+    tree = html.fromstring(request.content)
+
+    return (tree.xpath(i_xpath)[0])
+
+
+def merge_company_name(i_url, i_xpath_name, i_xpath_end):
+
+    company_name = get_company(i_url=i_url, i_xpath=i_xpath_name)
+
+    company_end = get_company(i_url=i_url, i_xpath=i_xpath_end)
+
+    print(company_name, company_end)
+
+
+def write_data(i_file, i_xpath):
     output_file(i_file=i_file,
-                i_url=i_url,
                 i_xpath=i_xpath,
-                i_first_page=i_first_page,
-                i_last_page=i_last_page,
-                i_sort=i_sort)
+                i_option=globals.option)
+
 
 def check_file(i_file, i_review):
-    
+
     if os.path.exists(i_file):
-        
+
         title_reviews = open(i_file, "r")
         title_lines = title_reviews.readlines()
         line_counter = 0
-        
+
         for lines in title_lines:
             line_counter += 1
-        
+
         if any(line_counter for i in i_review):
-            
+
             print(f'OK - {line_counter} lines found in {title_reviews.name}')
-        else:   
+        else:
             print(f'Warning - {line_counter} lines found in {title_reviews.name}')
     else:
         print('File not found')
-    
-    
-#get_company(globals.url, globals.company_name) #- successfully tested
-write_data(globals.file_title, globals.url, globals.title, globals.first_page, globals.last_page, globals.sort) #- successfully tested
-#write_data(globals.file_date, globals.url, globals.datetime, globals.first_page, globals.last_page, globals.sort)
-#write_data(globals.file_score, globals.url, globals.score, globals.first_page, globals.last_page, globals.sort)
-#write_data(globals.file_stars, globals.url, globals.star, globals.first_page, globals.last_page, globals.sort)
-#write_data(globals.file_recom, globals.url, globals.recommended, globals.first_page, globals.last_page, globals.sort)
-check_file(globals.file_title, globals.reviews) #- successfully tested
+
+merge_company_name(globals.url, globals.company_name, globals.company_end) #- successfully tested
+#rite_data(globals.file_title, globals.title) #- successfully tested
+#write_data(globals.file_date, globals.datetime)
+#write_data(globals.file_score, globals.score)
+#write_data(globals.file_stars, globals.star)
+#write_data(globals.file_recom, globals.recommended)
+#write_data(globals.file_depmt, globals.department)
+#check_file(globals.file_title, globals.reviews) #- successfully tested
--- globals.py
+++ globals.py
@@ -1,3 +1,6 @@
+import requests
+import sys
+from lxml import html
 # +++++++++++++++++++++++++++++++++++++++++++++
 # This file will be included into functions.py
 # It contains global variables
@@ -5,33 +8,47 @@
 
 # MAIN URL
 # e. g. https://www.kununu.com/de/COMPANY-NAME/kommentare/
-url = 'https://www.kununu.com/de/wuerth-industrie-service/kommentare/'
+url = 'https://www.kununu.com/de/pass-consulting-group/kommentare/'
+
+if not url:
+    sys.exit("Please initialize the URL variable!")
+
+if not requests.get(url).status_code == 200:
+    sys.exit(f'Page {url} not found, exiting...')
 
 # FILES
 file_title = "titles.txt"
-file_date  = "dates.txt"
+file_date = "dates.txt"
 file_score = "score.txt"
 file_stars = "stars.txt"
 file_recom = "recomms.txt"
+file_depmt = "depart.txt"
 
 # XPATH
-reviews		= '//div[@class="index__summaryLine__1Y3Gv"]/h2[@class="index__totalReviews__Fcmkx p-small-semibold"]//text()'
-company_name	= '//div[@class="index__nameStart__1-yIw"]//text()'
-title		= '//h3[@class="index__title__2uQec h3-semibold"]//text()'
-datetime	= '//span[@class="index__dateBlock__372Rm"]/time[@class="p-tiny-regular text-dark-63"]/@datetime'
-score		= '//div[@class="index__block__36tsj index__scoreBlock__138n3"]//text()'
-star		= '//div[@class="index__rating__3nC2L"]/div[@class="index__ratingBlock__lNDrc"]/div[@class="index__block__36tsj index__scoreBlock__138n3"]/span[@class="index__stars__2ads4 index__large__1Ez3-"]//@data-score'
-recommended	= '//span[@class="p-tiny-bold"]//text()'      
+reviews = '//div[@class="index__summaryLine__1Y3Gv"]/h2[@class="index__totalReviews__Fcmkx p-small-semibold"]//text()'
+company_name = '//div[@class="index__nameStart__1-yIw"]//text()'
+company_end = '//div[@class="index__nameEnd__DIV05"]//text()'
+title = '//h3[@class="index__title__2uQec h3-semibold"]//text()'
+datetime = '//span[@class="index__dateBlock__372Rm"]/time[@class="p-tiny-regular text-dark-63"]/@datetime'
+score = '//div[@class="index__block__36tsj index__scoreBlock__138n3"]//text()'
+star = '//div[@class="index__rating__3nC2L"]/div[@class="index__ratingBlock__lNDrc"]/div[@class="index__block__36tsj index__scoreBlock__138n3"]/span[@class="index__stars__2ads4 index__large__1Ez3-"]//@data-score'
+recommended = '//span[@class="p-tiny-bold"]//text()'
+department = '//span[@class="index__sentence__3PKUg text-dar-63 index__middot__3vlu3"]//text()'
 
 # OTHERS
-sort		= '?sort=oldest'	# '?sort=newest'; '?sort=best', '?sort=worst'
-first_page	= 1
-last_page       = 40
+sort = '?sort=oldest'	# '?sort=newest', '?sort=best', '?sort=worst'
+option = 'file'                 # 'cli', 'file'
 
 # PAGE CALCULATOR
 # Each page contains 10 entries
-#est_last_page = int((reviews / 10))
-#remainder = est_last_page % 1
+request = requests.get(url)
+tree = html.fromstring(request.content)
+section = tree.xpath(reviews)
+from_list_to_str = section[0][:-13]
+count_reviews = int(from_list_to_str)
 
-# PAGES
-#pages = est_last_page + (1 - remainder) + 1 
+est_last_page = (count_reviews / 10)
+remainder = est_last_page % 1
+
+first_page = 1
+last_page = int(est_last_page + (1 - remainder) + 1)
